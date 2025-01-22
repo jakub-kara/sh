768edc3d7b3b95fe9370106a6cb40b9cb00fb7da
joecooper98 <newc6739@lambda.chem.ox.ac.uk>
2025-01-22 18:16:30 +0000

    reimplementation of molcas

--- electronic/molcas.py
+++ electronic/molcas.py
@@ -8,24 +8,15 @@ class Molcas(ESTProgram, key = "molcas"):
     def _select_method(self, key):
         methods = {
             "cas": self.cas,
+            "pt2": self.pt2,
         }
         return methods[key]
 
     def execute(self):
-        err = os.system(f"molpro -W . -I . -d ./tmp_molpro -s {self._file}.inp")
-        if err > 0:
-            raise InterruptedError(f"Error in MOLPRO, exit code {err}")
-
-        err = os.system(f"pymolcas -f -b1 molcas.input")
+        err = os.system(f"pymolcas -f -b1 {self._file}.input")
         if err:
             raise InterruptedError(f"Error in MOLCAS, exit code {err}")
 
-        for i in range(self.n_states):
-            for j in range(i + 1, self.n_states):
-                if self._calc_nac[i, j]:
-                    err = os.system(f"pymolcas -f -b1 molcas_{i}_{j}.input")
-                    if err:
-                        raise InterruptedError(f"Error in MOLCAS, exit code {err}")
 
     def backup_wf(self):
         os.system(f"cp est/{self._file}.wf backup/")
@@ -34,23 +25,50 @@ class Molcas(ESTProgram, key = "molcas"):
         os.system(f"cp backup/{self._file}.wf est/")
 
     def cas(self):
-        self._create_input_main()
+        self._create_input_stem()
+
+        for i in range(self.n_states):
+            if self._calc_grad[i]:
+                self._add_input_ddr(i,i)
 
         for i in range(self.n_states):
             for j in range(i + 1, self.n_states):
                 if self._calc_nac[i, j]:
-                    self._create_input_nac(i, j)
+                    self._add_input_ddr(i,j)
 
-        for i, j, val in read_output_molcas_ham('molcas.log', traj.est.config):
-            if i-skip >= 0 and j-skip >= 0 and i-skip < traj.par.n_states and j-skip < traj.par.n_states:
-                traj.pes.ham_diab_mnss[-1,0,i-skip,j-skip] = val
+        self._add_input_rassi(False)
 
-        for s1 in range(traj.par.n_states):
-            for s2 in range(i + 1, traj.par.n_states):
-                if traj.est.calculate_nacs[i, j]:
-                    for i, j, a, val in read_output_molcas_nac(f"molcas_{i}_{j}.log", i, j): traj.pes.nac_ddr_mnssad[-1,0,i-skip,j-skip,a] = val
+    def pt2(self):
+        self._create_input_stem()
 
-    def _create_input_main(self):
+        self._add_input_pt2()
+
+        for i in range(self.n_states):
+            if self._calc_grad[i]:
+                self._add_input_ddr(i,i)
+
+        for i in range(self.n_states):
+            for j in range(i + 1, self.n_states):
+                if self._calc_nac[i, j]:
+                    self._add_input_ddr(i,j)
+
+        self._add_input_rassi(True)
+
+        #
+        #
+        # for i, j, val in read_output_molcas_ham(self._file+'.log', traj.est.config):
+        #     if i-skip >= 0 and j-skip >= 0 and i-skip < traj.par.n_states and j-skip < traj.par.n_states:
+        #         traj.pes.ham_diab_mnss[-1,0,i-skip,j-skip] = val
+        #
+        # for s1 in range(traj.par.n_states):
+        #     traj.pes.
+        #     for s2 in range(i + 1, traj.par.n_states):
+        #         if traj.est.calculate_nacs[i, j]:
+        #             for i, j, a, val in read_output_molcas_nac(f"molcas_{i}_{j}.log", i, j): traj.pes.nac_ddr_mnssad[-1,0,i-skip,j-skip,a] = val
+        #
+
+
+    def _create_input_stem(self):
         os.system('rm A_PT2 GAMMA*')
 
         with open(f"{self._file}.input", "w") as file:
@@ -64,7 +82,7 @@ class Molcas(ESTProgram, key = "molcas"):
             file.write("RICD\n")
             file.write(f"BASIS={self._options['basis']}\n\n")
 
-            file.write(">>> copy wf.wf JOBOLD\n")
+            file.write(f">>> copy {self._file}.wf JOBOLD\n")
 
             file.write("&SEWARD\n\n")
             file.write("&RASSCF\n")
@@ -77,36 +95,49 @@ class Molcas(ESTProgram, key = "molcas"):
             for i in range(self.n_states):
                 if self._calc_grad[i]:
                     file.write(f"RLXROOT={i+1}\n")
-            file.write(f"CIROOT={self.n_states} {self.n_states} 1\n\n")
+            file.write(f"CIROOT={self._options['sa']} {self._options['sa']} 1\n\n")
 
             file.write(">>> COPY JOB001 JOB002\n")
             file.write(">>> COPY molcas.JobIph JOB001\n")
             file.write(">>> COPY molcas.JobIph JOBOLD\n")
-            file.write(">>> COPY molcas.JobIph wf.wf\n")
+            file.write(f">>> COPY molcas.JobIph {self._file}.wf\n")
 
-            if self._options['type'] == 'caspt2':
-                file.write(f"&CASPT2\n")
-                file.write(f"GRDT\n")
-                file.write(f"imag={self._options['imag']}\n")
-                file.write(f"shift={self._options['shift']}\n")
-                file.write(f"ipea={self._options['ipea']}\n")
-                file.write(f"sig2={self._options['sig2']}\n")
-                file.write(f"threshold =  1e-8 1e-6\n")
-                if self._options['ipea'] > 0:
-                    file.write('DORT\n')
-                if self._options["ms_type"].upper()[0] == 'R':
-                    file.write(f"RMUL=all\n\n")
-                elif self._options["ms_type"].upper()[0] == 'M':
-                    file.write(f"MULT=all\n\n")
-                else:
-                    file.write(f"XMUL=all\n\n")
-                file.write(">>> COPY molcas.JobMix JOB001\n\n")
+    def _add_input_ddr(self, i: int, j: int):
+        with open(f"{self._file}.input", "a") as file:
+            file.write(f"&ALASKA\n")
+            if i==j:
+                file.write(f"root = {i+1}\n\n")
+            else:
+                file.write(f"nac = {i+1} {j+1}\n\n")
 
-            file.write(f"&ALASKA\n\n")
 
+    def _add_input_pt2(self):
+        with open(f"{self._file}.input", "a") as file:
+            file.write(f"&CASPT2\n")
+            file.write(f"GRDT\n")
+    
+            file.write(f"imag={self._options['imag']}\n")
+            file.write(f"shift={self._options['shift']}\n")
+            file.write(f"ipea={self._options['ipea']}\n")
+            file.write(f"sig2={self._options['sig2']}\n")
+            file.write(f"threshold =  1e-8 1e-6\n")
+            if self._options['ipea'] > 0:
+                file.write('DORT\n')
+            if self._options["ms_type"].upper()[0] == 'R':
+                file.write(f"RMUL=all\n\n")
+            elif self._options["ms_type"].upper()[0] == 'M':
+                file.write(f"MULT=all\n\n")
+            else:
+                file.write(f"XMUL=all\n\n")
+            file.write(">>> COPY molcas.JobMix JOB001\n\n")
+
+
+
+    def _add_input_rassi(self, pt2:bool):
+        with open(f"{self._file}.input", "a") as file:
             file.write(f"&RASSI\n")
 
-            if self._options.get("caspt2", False):
+            if pt2:
                 file.write("EJOB\n")
             #  if SOC:
                 #  f.write("SPIN\nSOCO=0\n")
@@ -114,59 +145,9 @@ class Molcas(ESTProgram, key = "molcas"):
             file.write("STOVERLAPS\n")
 
 
-    def _create_input_nac(self, nacidx1: int, nacidx2: int):
-        with open(f"{self._file}_{nacidx1}_{nacidx2}.input", "w") as file:
-            file.write("&GATEWAY\n")
-            file.write(f"COORD={self._file}.xyz\n")
-            #  if SOC:
-                #  f.write("AMFI")
-            # Need to change to angstrom
-
-            file.write("GROUP=NOSYM\n")
-            file.write("RICD\n")
-            file.write(f"BASIS={self._options['basis']}\n\n")
-            file.write("&SEWARD\n\n")
-            file.write("&RASSCF\n")
-            file.write("JOBIPH\nCIRESTART\n")
-            nactel = self._options['nel'] - 2 * self._options['closed']
-            file.write(f"NACTEL={nactel} 0 0\n")
-            ras2 = self._options['active'] - self._options['closed']
-            file.write(f"RAS2={ras2}\n")
-            file.write(f"INACTIVE={self._options['closed']}\n")
-            for i in range(self.n_states):
-                if self._calc_grad[i, i]:
-                    file.write(f"RLXROOT={i+1}\n")
-            file.write(f"CIROOT={self.n_states} {self.n_states} 1\n\n")
-
-            file.write(">>> COPY molcas.JobIph JOB001\n")
-
-            if self._options['type'] == 'caspt2':
-                file.write(f"&CASPT2\n")
-                file.write(f"GRDT\n")
-                if nacidx1 != nacidx2:
-                    file.write(f"NAC = {nacidx1 + 1} {nacidx2 + 1}\n")
-                file.write("convergence = 1e-8\n")
-                file.write(f"imag={self._options['imag']}\n")
-                file.write(f"shift={self._options['shift']}\n")
-                file.write(f"ipea={self._options['ipea']}\n")
-                file.write(f"sig2={self._options['sig2']}\n")
-                if self._options['ipea'] > 0:
-                    file.write('DORT\n')
-                if self._options["ms_type"].upper()[0] == 'R':
-                    file.write(f"RMUL=all\n\n")
-                elif self._options["ms_type"].upper()[0] == 'M':
-                    file.write(f"MULT=all\n\n")
-                else:
-                    file.write(f"XMUL=all\n\n")
-                file.write(">>> COPY molcas.JobMix JOB001\n\n")
-
-            file.write(f"&ALASKA\n")
-            if nacidx1 != nacidx2:
-                file.write(f"NAC = {nacidx1+1} {nacidx2+1}\n")
-
     def read_ham(self):
         ham = np.zeros((self._nstates, self._nstates), dtype=np.complex128)
-        with open('molcas.log', 'r') as file:
+        with open(self._file+'.log', 'r') as file:
             while True:
                 line = file.readline()
                 if not line:
@@ -177,17 +158,17 @@ class Molcas(ESTProgram, key = "molcas"):
                 if False: #SOC
                     pass
                 else:
-                    if self._options["type"] == 'caspt2':
-                        if 'ms-caspt2 energies' in line:
+                    if self._type == 'pt2':
+                        if 'ms-caspt2 energies' in line or 'xdw-caspt2 energies' in line:
                             #  while (line := file.readline()):
-                            for i in range(self._options['sa']):
+                            for i in range(self._nstates):
                                 data = file.readline().strip().split()
-                                yield int(data[-4])-1, int(data[-4])-1, float(data[-1])
+                                ham[int(data[3])-1, int(data[3])-1] = float(data[-1])
 
                     else:
                         if 'final state energy(ies)' in line:
                             for i in range(2): file.readline()
-                            for i in range(self._options['sa']):
+                            for i in range(self._nstates):
                             #  while (line := file.readline()):
                                 line = file.readline()
                                 data = line.strip().split()
@@ -196,49 +177,67 @@ class Molcas(ESTProgram, key = "molcas"):
 
     def read_grad(self):
         grad = np.zeros((self._nstates, self._natoms, 3))
-        with open('molcas.log', 'r') as file:
+        for s1 in range(self._nstates):
+            if self._calc_grad[s1]:
+                grad[s1] = self._read_single_grad(s1)
+        return grad
+
+    def _read_single_grad(self, i):
+        grad = np.zeros((self._natoms, 3))
+        with open(self._file+'.log', 'r') as file:
             while True:
                 line = file.readline()
                 if not line:
                     break
 
 
-                if 'RLXROOT' in line:
-                    state = int(line.split('=')[-1]) - 1
+                if f'Lagrangian multipliers are calculated for state no.   {i+1}' in line:
+                    #maybe doesn't work for i>=10
+                    while True:
+                        line = file.readline()
+                        if 'Molecular gradients' in line:
+                            for i in range(7): file.readline()
 
-                if 'Molecular gradients' in line:
-                    for i in range(7): file.readline()
-
-                    a = 0
-                    while len(data := file.readline().split()) > 1:
-                        grad[state, a] = [float(x) for x in data[1:]]
-                        a+=1
+                            a = 0
+                            while len(data := file.readline().split()) > 1:
+                                grad[a] = [float(x) for x in data[1:]]
+                                a+=1
+                            break
+                    break
         return grad
 
     def read_nac(self):
         nac = np.zeros((self._nstates, self._nstates, self._natoms, 3))
         for i in range(self.n_states):
             for j in range(i+1, self.n_states):
-                nac[i,j] = self._read_single_nac(i, j)
-                nac[j,i] = -nac[i,j]
-
+                if self._calc_nac[i,j]:
+                    nac[i,j] = self._read_single_nac(i, j)
+                    nac[j,i] = -nac[i,j]
+        return nac
 
 
     def _read_single_nac(self, i, j):
         nac = np.zeros((self._natoms, 3))
-        with open(self._file, 'r') as file:
+        with open(self._file+'.log', 'r') as file:
             while True:
                 line = file.readline()
                 if not line:
                     break
 
-                if 'Total derivative coupling' in line or 'Molecular gradients' in line:
-                    for i in range(7): file.readline()
 
-                    a = 0
-                    while len(data := file.readline().split()) > 1:
-                        nac[a] = [float(x) for x in data[1:]]
-                        a += 1
+                if f'Lagrangian multipliers are calculated for states no.   {i+1}/  {j+1}' in line:
+                    #maybe doesn't work for i>=10
+                    while True:
+                        line = file.readline()
+                        if 'Total derivative coupling' in line:
+                            for i in range(7): file.readline()
+
+                            a = 0
+                            while len(data := file.readline().split()) > 1:
+                                nac[a] = [float(x) for x in data[1:]]
+                                a+=1
+                            break
+                    break
         return nac
 
     def read_dipmom(self):
@@ -265,52 +264,22 @@ class Molcas(ESTProgram, key = "molcas"):
         return dipmom
 
     # probably change args
-    def read_ovlp(self, atoms, geom1, geom2):
-        """wrapper function to run all calculations from given inputs"""
-        self._move_old_files()
-        self._get_ci_and_orb_molpro('lumorb_a', 'dets_a')
-
-        input_file_string = '''
-    a_mo=lumorb_a.old
-    b_mo=lumorb_a
-    a_mo_read=1
-    b_mo_read=1
-    a_det=dets_a.old
-    b_det=dets_a
-    ao_read = 0
-    '''
-
-        with open('wf.inp', 'w') as f:
-            f.write(input_file_string)
-
-        self._get_ao_ovlp(atoms, geom1, geom2)
-
-        # TODO add wfoverlap path...
-        err = os.system('$SHARC/wfoverlap.x -m 2000 -f wf.inp > wf.out')
-
-        if err < 0:
-            sys.exit()
-
-
-        os.system('rm molpro_overlap*')
-        S_mat = self._read_wf_overlap('wf.out')
-        return S_mat
-
-def read_output_molcas_prop(file_name: str, config: dict):
-    ovlp = np.zeros((config['sa']*2,config['sa']*2))
-    with open(file_name, 'r') as file:
-        for line in file:
-            if 'OVERLAP MATRIX FOR THE ORIGINAL' in line:
-                file.readline()
-                for i in range(config['sa']*2):
-                    data = file.readline().split()
-                    for j in range(i//5):
-                        data += file.readline().split()
-
-                    ovlp[i,:len(data)] = [float(q) for q in data]
-
-        ovlp =ovlp[config['sa']:,:config['sa']]
-        U,_,Vt = np.linalg.svd(ovlp)
-        ovlp = U@Vt
-
-    return ovlp
+    def read_ovlp(self, dummy0, dummy1, dummy2):
+        ovlp = np.zeros((self._options['sa']*2,self._options['sa']*2))
+        with open(self._file+'.log', 'r') as file:
+            for line in file:
+                if 'OVERLAP MATRIX FOR THE ORIGINAL' in line:
+                    file.readline()
+                    for i in range(self._options['sa']*2):
+                        data = file.readline().split()
+                        for j in range(i//5):
+                            data += file.readline().split()
+    
+                        ovlp[i,:len(data)] = [float(q) for q in data]
+    
+            ovlp =ovlp[self._options['sa']:,:self._options['sa']]
+            ovlp =ovlp[:self._nstates,:self._nstates]
+            U,_,Vt = np.linalg.svd(ovlp)
+            ovlp = U@Vt
+    
+        return ovlp
